f721ae33136e6cdd6cb9174a1a0810b8a57d71bd
Dams2312 <225397863+Dams2312@users.noreply.github.com>
2025-08-21 08:38:51 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+"""
+Nombre: Danny Julian Velasco Olarte, Jefferson Andres Amarillo Rios
+Fecha: 2023-10-30
+Descripcion: Desarrollo de programa de inscripción y acompañamiento los campers en su proceso de formación
+"""
