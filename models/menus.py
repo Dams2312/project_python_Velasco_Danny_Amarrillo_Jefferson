74cc9a15beb75b4e69d70fc4a651058784aa8d8f
Amarillo2410 <225398294+Amarillo2410@users.noreply.github.com>
2025-08-22 07:15:22 -0500

    menus and register models

--- models/menus.py
+++ models/menus.py
@@ -1,1 +1,34 @@
 #crea los menus que sean necesarios para el programa
+def crear_menus():
+    print(""" 
+    -------------------------------------------------
+                    WELCOME TO CAMPUS 
+    -------------------------------------------------
+    1.ingresar 
+    2.registrarse
+    3.salir
+    -------------------------------------------------
+    Elija una opcion:
+    """
+    ,end="")
+    opcion = int(input())
+    return opcion
+
+
+def ingresar_menu():
+    print("""
+    -------------------------------------------------
+                    INGRESAR 
+    -------------------------------------------------
+    1.ingresar como camper
+    2.ingresar como traienr
+    3.ingresar como coordinador
+    4.salir
+    -------------------------------------------------
+    Elija una opcion:
+    """
+    ,end="")
+    opcion = int(input())
+    return opcion
+
+
--- models/registro.py
+++ models/registro.py
@@ -1,1 +1,32 @@
 #modulo encargado de aser el registro de los campers
+def registro_camper():
+    print("""
+    -------------------------------------------------
+                    REGISTRO CAMPER 
+    -------------------------------------------------
+    Por favor, ingrese los siguientes datos:     
+    Nombre: """, end="")
+    nombre = input()
+    print("Apellido: ", end="")
+    apellido = input()
+    print("Edad: ", end="")
+    edad = int(input())
+    print("Correo electrónico: ", end="")
+    correo = input()
+    print("Teléfono: ", end="")
+    telefono = input()
+    print("direccion ", end="")
+    direccion = input()
+    print ("acudiente: ",end="")
+    acudiente = input()
+    print("numero de identificacion: ", end="")
+    identificacion = input()
+    
+    # aqui es donde pedimos los datos del camper y para poder hacer su registro con datos "basicos" para saber quien es 
+    
+    print(f"""
+    -------------------------------------------------
+                    REGISTRO EXITOSO 
+    -------------------------------------------------
+    Bienvenido {nombre} {apellido}, tu registro ha sido exitoso.
+    """)
